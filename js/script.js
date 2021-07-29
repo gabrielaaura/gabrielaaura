b37884c5470d6a56412c8246d954c1fb212ed198
gabriela.aura <pedro.hedro@globalhitss.com.br>
2021-07-29 17:56:37 -0300

    remoção um slick do arquivo js

--- js/script.js
+++ js/script.js
@@ -22,28 +22,4 @@ $('.zigzag-container').slick({
     ]
   });
 
-  $('.center').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+  
